f7b86471264a1f1061d324c81c2e66787d1b7c02
Cosmin Poieana <cmin@ropython.org>
2014-10-21 01:53:38 +0300

    Problems update

--- solveio/solveio/problems/a.py
+++ solveio/solveio/problems/a.py
@@ -1,11 +1,11 @@
-# Sa se calculeze suma numerelor primite ca intrare,
-# dar si media aritmetica si geometrica.
-# Sa se returneze o lista cu aceste rezultate
+# Sa se calculeze suma numerelor din lista primita ca intrare,
+# dar si media aritmetica si geometrica a acestora.
+# Sa se returneze o tupla cu aceste rezultate
 # in ordinea precizata mai sus.
 # Numerele sunt naturale si nenule, iar lista de intrare are
 # cel putin un element.
 #
-# Hints:
+# Sugestii:
 #    - folositi functia `round` pentru a pastra doar primele doua zecimale
 
 
@@ -14,6 +14,6 @@ def compute(data):
 
 
 if __name__ == "__main__":
-    assert compute([1, 2, 3]) == [6, 2, 1.82]
-    assert compute([2, 8]) == [10, 5, 4]
-    assert compute([1, 1, 1, 1]) == [4, 1, 1]
+    assert compute([1, 2, 3]) == (6, 2, 1.82)
+    assert compute([2, 8]) == (10, 5, 4)
+    assert compute([1, 1, 1, 1]) == (4, 1, 1)
