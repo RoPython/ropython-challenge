1a86a0626de1cff366cfae4330af916d32e201ba
Cosmin Poieana <cmin@ropython.org>
2014-10-20 18:40:53 +0300

    Add a new problem (with hidden solution and i/o file)

---
+++ solveio/problems/a.py
@@ -0,0 +1,20 @@
+# Sa se calculeze suma numerelor primite ca intrare,
+# dar si media aritmetica si geometrica.
+# Sa se returneze o lista cu aceste rezultate
+# in ordinea precizata mai sus.
+# Numerele sunt naturale si nenule, iar lista de intrare are
+# cel putin un element.
+#
+# Hints:
+#    - folositi functia `round` pentru a pastra doar primele doua zecimale
+
+
+
+def calcul(intrare):
+    pass
+
+
+if __name__ == "__main__":
+    assert calcul([1, 2, 3]) == [6, 2, 1.81]
+    assert calcul([2, 8]) == [10, 5, 4]
+    assert calcul([1, 1, 1, 1]) == [4, 1, 1]
