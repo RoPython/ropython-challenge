2b425a3c7affb05be7f79234d3cf94f40448571c
Alexandru Coman <acoman@cloudbasesolutions.com>
2014-10-20 21:52:55 +0300

    Add plugin for evaluator.

--- solveio/solveio/api/__init__.py
+++ solveio/solveio/api/__init__.py
@@ -1,4 +1,14 @@
+import cherrypy
+
+from solveio.util import UserManager
+
 from ._exercise import Exercise
+from ._plugins import Evaluator
 from ._solution import Solution
 from ._submit import Submit
 from ._top import Top
+
+cherrypy.tools.user_required = UserManager()
+
+evaluator = Evaluator(cherrypy.engine)
+evaluator.subscribe()
